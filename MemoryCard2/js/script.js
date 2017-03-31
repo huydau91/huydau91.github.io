4723cbe0bb4385acf8d3b0c7fab149c0e00c2e91
mrbean <dauthehuy91@gmail.com>
2017-03-31 13:10:44 +0700

    update

--- MemoryCard2/js/script.js
+++ MemoryCard2/js/script.js
@@ -1,7 +1,10 @@
 var cards = ['luffy', 'zoro', 'nami', 'law', 'ace', 'frank', 'robin', 'shank', 'sanji', 'hawkeye', 'kid', 'brook'];
 var current = null;
 var count = 0;
-var time = 30;
+var remainingtime = 100;
+var runningtime = 0;
+var i = 0;
+var a = [];
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -29,25 +32,80 @@ $(function(){
 				'<div class="back"><img src="img/' + cards[i] + '.jpg" class="img-responsive"></div></div>'
 	};
 	$('.grid').html(loadcards);
+});
 
+
+function normal(){
+	var time = 90;
+	var rate = 100/90;
+	$('.batdau').hide();
+	$('.batdau').css('z-index', '0');
+	$('#nhacnen').trigger('play');
 	var run = setInterval(function(){
+		$('.timer').width(remainingtime + '%');
+		runningtime += rate;
+		remainingtime = 100 - runningtime;
 		time--;
 		console.log(time);
 
-		if(time == 0){
-			clearInterval(run);
-			// $('#thongbao').modal('open');
-			// loadcards = "Bạn đã thua cuộc, số điểm của bạn là: " + count;
-			// $('.grid').html(loadcards);
-			alert("Bạn đã thua cuộc, số điểm của bạn là: " + count);
-			$('.card').css('pointer-events', 'none');
+	if(time < 0){
+		clearInterval(run);
+		$('#nhacnen').trigger('pause');
+		$('#hetgio').trigger('play');
+		$('.card').css('pointer-events', 'none');
+		$(".lose > .dialog > h1").html("YOUR SCORE: " + count);
+		$('.lose').show();
 		}
+
+	if(count == 12){
+		clearInterval(run);
+		$('#nhacnen').trigger('pause');
+		$('#victory').trigger('play');
+		$('.card').css('pointer-events', 'none');
+		$(".win > .dialog > h1").html("YOU ARE VICTORY!!!");
+		$('.win').show();
+	};
+
 	}, 1000);
-});
+};
+
+function hard(){
+	var time = 60;
+	var rate = 100/60;
+	$('.batdau').hide();
+	$('.batdau').css('z-index', '0');
+	$('#nhacnen').trigger('play');
+	var run = setInterval(function(){
+		$('.timer').width(remainingtime + '%');
+		runningtime += rate;
+		remainingtime = 100 - runningtime;
+		time--;
+		console.log(time);
+
+	if(time < 0){
+		clearInterval(run);
+		$('#nhacnen').trigger('pause');
+		$('#hetgio').trigger('play');
+		$('.card').css('pointer-events', 'none');
+		$(".lose > .dialog > h1").html("YOUR SCORE: " + count);
+		$('.lose').show();
+		}
+
+	if(count == 12){
+		clearInterval(run);
+		$('#nhacnen').trigger('pause');
+		$('#victory').trigger('play');
+		$('.card').css('pointer-events', 'none');
+		$(".win > .dialog > h1").html("YOU ARE VICTORY!!!");
+		$('.win').show();
+	};
+	}, 1000);
+};
 
 $(function(){
 	$('.card').click(function(){
 	
+	$('#click').trigger('play');
 	$(this).toggleClass('flipped');
 	$(this).css('pointer-events', 'none');
 
@@ -57,26 +115,34 @@ $(function(){
 		current = checked;
 	}
 	else{
-		if(current.attr('name') != $(checked).attr('name')){
+		if(current.attr('name') != checked.attr('name')){
 			setTimeout(function(){
 				checked.toggleClass('flipped');
 				current.toggleClass('flipped');
+				$('#sai').trigger('play');
+				current.css('pointer-events', 'auto');
+				checked.css('pointer-events', 'auto');
+				// $('.grid').css('pointer-events', 'auto');
+
 				current = null;
 			}, 500);
-			$('.card').css('pointer-events', 'auto');
 		}
 		else{
 			setTimeout(function(){
 				checked.css('opacity', '0');
-				checked.css('pointer-events', 'none');
 				current.css('opacity', '0');
+				// checked.fadeOut('normal');
+				// current.fadeOut('normal');
+				$('#dung').trigger('play');
+
+				// $('.grid').css('pointer-events', 'auto');
+				checked.css('pointer-events', 'none');
 				current.css('pointer-events', 'none');
+
 				current = null;
 
 				count++;
-
-				if(count == 12) alert("Chúc mừng bạn đã qua màn!!!");
-			}, 200);
+			}, 500);
 		}
 	}
 	});
