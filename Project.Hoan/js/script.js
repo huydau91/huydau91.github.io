b904321ee897c4b8ecf92f6305c2d223c52447d7
huydau91 <dauthehuy91@gmail.com>
2017-05-09 23:31:15 +0700

    update

--- Project.Hoan/js/script.js
+++ Project.Hoan/js/script.js
@@ -1,6 +1,13 @@
 function loadanh(){
-	for(var i = 1; i <= 24; i++){
-		$("#loadanh").append('<a href="#"><div class="col-sm-4">' + '<img src="img/' + i + '.jpg"></div></a>')
+	for(var i = 1; i <= 8; i++){
+		$(".phaotrangtri").append('<div class="col-sm-3">' + '<a href="#"><img src="img/phao' + i + '.jpg"></a>' +
+					'<p class="code">912 NV</p><p class="price">$5</p><p class="save"><span>Add to cart</span></p>' +
+				'</div>')
+	}
+	for(var i = 1; i <= 8; i++){
+		$(".bogiay").append('<div class="col-sm-3">' + '<a href="#"><img src="img/bogiay' + i + '.jpg"></a>' +
+					'<p class="code">912 NV</p><p class="price">$5</p><p class="save"><span>Add to cart</span></p>' +
+				'</div>')
 	}
 }
 
